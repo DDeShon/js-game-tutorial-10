1872aff40b8c558245fc8773a060619db5105bd0
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-09 21:30:50 -0500

    working on player class and movement

--- script.js
+++ script.js
@@ -31,7 +31,65 @@ window.addEventListener("load", function () {
     }
   }
 
-  class Player {}
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.gameHeight - this.height;
+      this.image = document.getElementById("playerImage");
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 0;
+      this.yVelocity = 0;
+      this.weight = 1;
+    }
+    draw(context) {
+      context.fillStyle = "white";
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+    update(input) {
+      if (input.keys.indexOf("ArrowRight") > -1) {
+        this.speed = 5;
+      } else if (input.keys.indexOf("ArrowLeft") > -1) {
+        this.speed = -5;
+      } else if (input.keys.indexOf("ArrowUp") > -1 && this.onGround()) {
+        this.yVelocity -= 32;
+      } else {
+        this.speed = 0;
+      }
+      // horizontal movement
+      this.x += this.speed;
+      if (this.x < 0) this.x = 0;
+      else if (this.x > this.gameWidth - this.width)
+        this.x = this.gameWidth - this.width;
+      // vertical movement
+      this.y += this.yVelocity;
+      if (!this.onGround()) {
+        this.yVelocity += this.weight;
+      } else {
+        this.yVelocity = 0;
+      }
+      if (this.y > this.gameHeight - this.height)
+        this.y = this.gameHeight - this.height;
+    }
+    onGround() {
+      return this.y >= this.gameHeight - this.height;
+    }
+  }
 
   class Background {}
 
@@ -42,6 +100,13 @@ window.addEventListener("load", function () {
   function displayStatusText() {}
 
   const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
 
-  function animate() {}
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw(ctx);
+    player.update(input);
+    requestAnimationFrame(animate);
+  }
+  animate();
 });
