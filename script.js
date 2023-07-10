1b6488d0715d8d9a9d087df8f5fa14eaea39644a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-10 18:48:30 -0500

    working on enemy class

--- script.js
+++ script.js
@@ -48,8 +48,6 @@ window.addEventListener("load", function () {
       this.weight = 1;
     }
     draw(context) {
-      //   context.fillStyle = "white";
-      //   context.fillRect(this.x, this.y, this.width, this.height);
       context.drawImage(
         this.image,
         this.frameX * this.width,
@@ -63,6 +61,7 @@ window.addEventListener("load", function () {
       );
     }
     update(input) {
+      // remove movement on x axis
       if (input.keys.indexOf("ArrowRight") > -1) {
         this.speed = 5;
       } else if (input.keys.indexOf("ArrowLeft") > -1) {
@@ -103,7 +102,7 @@ window.addEventListener("load", function () {
       this.y = 0;
       this.width = 2400;
       this.height = 700;
-      this.speed = 5;
+      this.speed = 8;
     }
     draw(context) {
       context.drawImage(this.image, this.x, this.y, this.width, this.height);
@@ -132,6 +131,11 @@ window.addEventListener("load", function () {
       this.y = this.gameHeight - this.height - 20;
       this.frameX = 0;
       this.frameY = 0;
+      this.maxFrame = 5;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / this.fps;
+      this.speed = 8;
     }
     draw(context) {
       context.drawImage(
@@ -146,16 +150,30 @@ window.addEventListener("load", function () {
         this.height
       );
     }
-    update() {
-      this.x--;
+    update(deltaTime) {
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX >= this.maxFrame) this.frameX = 0;
+        else this.frameX++;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
+
+      this.x -= this.speed;
     }
   }
 
-  enemies.push(new Enemy(canvas.width, canvas.height));
-  function handleEnemies() {
+  function handleEnemies(deltaTime) {
+    if (enemyTimer > enemyInterval + randomEnemyInterval) {
+      enemies.push(new Enemy(canvas.width, canvas.height));
+      randomEnemyInterval = Math.random() * 1000 + 500;
+      enemyTimer = 0;
+    } else {
+      enemyTimer += deltaTime;
+    }
     enemies.forEach((enemy) => {
       enemy.draw(ctx);
-      enemy.update();
+      enemy.update(deltaTime);
     });
   }
 
@@ -165,14 +183,21 @@ window.addEventListener("load", function () {
   const player = new Player(canvas.width, canvas.height);
   const background = new Background(canvas.width, canvas.height);
 
-  function animate() {
+  let lastTime = 0;
+  let enemyTimer = 0;
+  let enemyInterval = 1000;
+  let randomEnemyInterval = Math.random() * 1000 + 500;
+
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     background.draw(ctx);
     background.update();
     player.draw(ctx);
     player.update(input);
-    handleEnemies();
+    handleEnemies(deltaTime);
     requestAnimationFrame(animate);
   }
-  animate();
+  animate(0);
 });
