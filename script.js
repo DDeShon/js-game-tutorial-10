fad8e370d561cef53837f3c1da1e1ab251f8a491
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-12 21:19:44 -0500

    working on touch inputs

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ window.addEventListener("load", function () {
   class InputHandler {
     constructor() {
       this.keys = [];
+      this.touchY = "";
+      this.touchThreshold = 30;
       window.addEventListener("keydown", (e) => {
         if (
           (e.key === "ArrowDown" ||
@@ -32,13 +34,25 @@ window.addEventListener("load", function () {
         }
       });
       window.addEventListener("touchstart", (e) => {
-        console.log(e.changedTouches[0].pageY);
+        this.touchY = e.changedTouches[0].pageY;
       });
       window.addEventListener("touchmove", (e) => {
-        console.log(e.changedTouches[0].pageY);
+        const swipeDistance = e.changedTouches[0].pageY - this.touchY;
+        if (
+          swipeDistance < -this.touchThreshold &&
+          this.keys.indexOf("swipe up") === -1
+        )
+          this.keys.push("swipe up");
+        else if (
+          swipeDistance > this.touchThreshold &&
+          this.keys.indexOf("swipe down") === -1
+        )
+          this.keys.push("swipe down");
       });
       window.addEventListener("touchend", (e) => {
-        console.log(e.changedTouches[0].pageY);
+        console.log(this.keys);
+        this.keys.splice(this.keys.indexOf("swipe up"), 1);
+        this.keys.splice(this.keys.indexOf("swipe down"), 1);
       });
     }
   }
